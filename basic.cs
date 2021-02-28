d9245f823508a41ce8220b0e628376114fab4f3c
Kraken <krakenbruh@gmail.com>
2021-02-28 11:06:02 -0800

    first commit

---
+++ basic.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world"); //a statement
+            Console.WriteLIne("Tomato"); //a statement
+        }
+    }
+}
